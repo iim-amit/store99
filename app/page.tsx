0d4de2c1897201b7aab52047bff7c272ef427a5c
iim-amit <6661kumaramit@gmail.com>
2025-09-23 01:13:29 +0530

    components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
+
 
 const Home = () => {
   return <div className='p-10' bg-color='red-200'>
     <h2 className='text-xl font-semibold '>Welcome to Store99!</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus error quos nemo. Error ea perspiciatis doloremque iure a assumenda aut nihil quaerat quis voluptates. Veniam exercitationem facilis quaerat eius commodi minima amet in cum fugiat vel, enim esse voluptas. Aut, accusantium reprehenderit molestias sed quas ex necessitatibus odit! Doloribus, voluptate.</p>
+    <Button variant="destructive">click me</Button>
   </div>;
 };
 
